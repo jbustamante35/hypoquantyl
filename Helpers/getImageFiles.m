6f1b0d6c7ab1baad79ba74ebec28ec7cdddd80aa
jbustamante35 <jbustamante35@gmail.com>
2018-03-20 20:48:16 -0500

    Functions for performing PCA components sweep. Cleaning up of a lot of class files.
    Former-commit-id: 86223a044abe4328f83eeb2b618f1d0c04232bfa

--- Helpers/getImageFiles.m
+++ Helpers/getImageFiles.m
@@ -1,75 +1,79 @@
 function [im_out, exptDir] = getImageFiles(varargin)
 %% getImageFiles: function to load images from directory
-% This function takes a directory and image extension type as input, and loads the images into a 
+% This function takes a directory and image extension type as input, and loads the images into a
 % cell array based on date created. [make this modifiable if needed]
-% 
+%
 % Usage:
 %   [im_out, exptDir] = getImageFiles(dir_in, im_ext, visualize)
-% 
-% Input: 
-%   im_ext: 
-%   sort_by: 
-%   visualize: 
-% 
+%
+% Input:
+%   im_ext: file extension of images
+%   sort_by: file property to sort images by
+%   visualize: show images loading from this function
+%
 % Output:
-%   im_out: 
-%   exptDir: 
-% 
+%   im_out: cell array of all images loaded 
+%   exptDir: path to images loaded from this function 
+%
 
-    if nargin == 0    
-        im_ext    = input(sprintf('Enter image extension: '), 's');
-        sort_by   = input(sprintf('Sort by what property? '), 's');
-        visualize = false;
-        currDir = pwd;
-        cd(uigetdir(currDir, 'Select directory containing images'));
-        exptDir = pwd;
-
-    elseif iscell(varargin)
-        expPrp    = varargin{1};
-        im_ext    = varargin{2};
-        sort_by   = varargin{3};
-        visualize = varargin{4};    
-        currDir   = pwd;
-
-        if isunix
-            exptDir = [expPrp.folder '/' expPrp.name];
-        else
-            exptDir = [expPrp.folder '\' expPrp.name];
-        end
-
-        cd(exptDir);
-
+if nargin == 0
+    im_ext    = input(sprintf('Enter image extension: '), 's');
+    sort_by   = input(sprintf('Sort by what property? '), 's');
+    visualize = false;
+    currDir = pwd;
+    cd(uigetdir(currDir, 'Select directory containing images'));
+    exptDir = pwd;
+    
+elseif iscell(varargin)
+    expPrp    = varargin{1};
+    im_ext    = varargin{2};
+    sort_by   = varargin{3};
+    visualize = varargin{4};
+    currDir   = pwd;
+    
+    if isunix
+        delim = '/';
     else
-        fprintf(2, 'Error with input parameters. Please check again.\n');
-        return;
+        delim = '\';
     end
+    
+    exptDir = [expPrp.folder delim expPrp.name];
+    cd(exptDir);
+    
+else
+    fprintf(2, 'Error with input parameters. Please check again.\n');
+    return;
+end
 
-    %% Go to image directory and store in sorted table 
-    imPath = dir(['*.' im_ext]);
-    imTble = struct2table(imPath);
-    imSort = sortrows(imTble, sort_by);
+%% Go to image directory and store in sorted table
+imPath = dir(['*.' im_ext]);
+imTble = struct2table(imPath);
+imSort = sortrows(imTble, sort_by);
+imName = table2struct(imSort);
+imFile = arrayfun(@(x) [x.folder delim x.name], imName, 'UniformOutput', 0);
 
-    %% Read images
-    im_out = cell(1, size(imSort, 1));
-    for i  = 1:length(im_out)
-        try
-            im_out{i} = imread(imSort.name{i});
-        catch 
-    %         fprintf(2, '%s (%s) \n', e.message, imSort.name{i});
-            continue;
-        end
+%% Read image files and remove empty cells
+im_out = cellfun(@(x) readOrReturn(x), imFile, 'UniformOutput', 0);
+im_out = im_out(~cellfun('isempty', im_out));
+cd(currDir);
+
+%% Iterate through images to verify correct order
+if visualize
+    figure;
+    for i = 1:length(im_out)
+        imagesc(im_out{i}), colormap gray, axis image, axis off;
+        drawnow;
     end
+end
 
-    im_out = im_out(~cellfun('isempty',im_out));
-    cd(currDir);
+end
 
-    %% Iterate through images to verify correct order
-    if visualize
-        figure;
-        for i = 1:length(im_out)
-            imagesc(im_out{i}), colormap gray, axis image, axis off;
-            pause(0.001);
-        end
-    end
+function im = readOrReturn(fin)
+%% readOrDie: function to read image file or return empty cell
+try
+    im = imread(fin);
+catch
+    im = {};
+end
 
 end
--- Helpers/randomContours.m
+++ Helpers/randomContours.m
@@ -1,4 +1,4 @@
-function CTRS = randomContours(exp_in, num_seeds, max_size, p, vis)
+function CTRS = randomContours(Ein, Nseeds, max_size, typ, p, vis)
 %% randomContours: obtain and normalize random set of contours
 % This function blah
 %
@@ -9,6 +9,7 @@ function CTRS = randomContours(exp_in, num_seeds, max_size, p, vis)
 %   exp: Experiment object to generate contour data
 %   num_seeds: number of random Seedlings to analyze
 %   max_size: number of coordinates to normalize boundaries
+%   typ: 0 to get contours of Seedlings, 1 to get contours of PreHypocotyl
 %   p: length of pause time between plotting figure
 %   vis: boolean to plot figures or not
 %
@@ -17,51 +18,39 @@ function CTRS = randomContours(exp_in, num_seeds, max_size, p, vis)
 %
 
 %% Initialize object array of Seedlings
-S    = exp_in.combineSeedlings;
-sIdx = randi(numel(S), 1, num_seeds);
-CTRS = repmat(ContourJB, 1, num_seeds);
+S    = Ein.combineSeedlings;
+sIdx = randi(numel(S), 1, Nseeds);
+CTRS = repmat(ContourJB, 1, Nseeds);
 
 %% Get contour at random frame from random Seedling 
-for i = 1 : num_seeds
-    rs      = S(sIdx(i));
-    rFrm    = randi(rs.getLifetime, 1);
-    im      = rs.getImageData(rFrm, 'gray');
-    bw      = rs.getImageData(rFrm, 'bw');
-    CTRS(i) = extractContour(bw, max_size);
-    
-%     [bnds, dL, L, I] = extractContour(bw, max_size);
-%     CTRS(i).Bounds  = bnds;
-%     CTRS(i).Dists   = dL;
-%     CTRS(i).Sums    = L;
-%     CTRS(i).Interps = I;
-
-    CTRS(i).setGrayImageAtFrame(im, 1);
-    CTRS(i).setBWImageAtFrame(bw, 1);
-    org = sprintf('%s_%s_%s_Frm%d', rs.ExperimentName, rs.GenotypeName, rs.getSeedlingName, rFrm);
-    CTRS(i).setOrigin(org);
+cIdx = 1;
+for k = sIdx        
+    rs         = S(k);
+    CTRS(cIdx) = getContour(rs, typ, max_size);
+    cIdx = cIdx + 1;
 end
 
 %% Visualize output if desired
 if vis
     figure;
-    for i = 1 : num_seeds
-        im = CTRS(i).getGrayImageAtFrame(1);
-        bw = CTRS(i).getBWImageAtFrame(1);
+    for k = 1 : Nseeds
+        im = CTRS(k).getGrayImageAtFrame(1);
+        bw = CTRS(k).getBWImageAtFrame(1);
         
         % Initial and Interpolated boundaries on grayscale image
         subplot(121);
         imagesc(im), colormap gray, axis image;
         hold on;
-        plot(CTRS(i).Bounds(:,2), CTRS(i).Bounds(:,1), 'rx');
-        plot(CTRS(i).Interps(:,2), CTRS(i).Interps(:,1), 'g.');
+        plot(CTRS(k).Bounds(:,2), CTRS(k).Bounds(:,1), 'rx');
+        plot(CTRS(k).Interps(:,2), CTRS(k).Interps(:,1), 'g.');
         hold off;
 
         % Initial and Interpolated boundaries on bw image
         subplot(122);
         imagesc(bw), colormap gray, axis image;
         hold on;
-        plot(CTRS(i).Bounds(:,2), CTRS(i).Bounds(:,1), 'rx');
-        plot(CTRS(i).Interps(:,2), CTRS(i).Interps(:,1), 'g.');
+        plot(CTRS(k).Bounds(:,2), CTRS(k).Bounds(:,1), 'rx');
+        plot(CTRS(k).Interps(:,2), CTRS(k).Interps(:,1), 'g.');
         hold off;
 
         pause(p);
@@ -71,6 +60,39 @@ end
 
 end
 
+function ctr = getContour(rs, tp, sz)
+%% getContour: subfunction to extract contour from image and store various data
+% This function takes in either a Seedling or Hypocotyl as input and outputs a ContourJB object and
+% stores grayscale and bw image data and its origin.
+% 
+% Input:
+%   rs: Seedling to extract contour
+%   tp: 0 to extract contour from Seedling, 1 to extract contour from that Seedling's Hypocotyl
+%   sz: 
+% 
+% Output:
+%   ctr: contour extracted from image at random frame 
+% 
+    % Random frame from Seedling's lifetime
+    rFrm    = randi(rs.getLifetime, 1);
+    
+    % Extract either Seedling or Hypocotyl image
+    if tp
+        im      = rs.getPreHypocotyl(rFrm).getImageData('gray');
+        bw      = rs.getPreHypocotyl(rFrm).getImageData('bw');
+    else
+        im      = rs.getImageData(rFrm, 'gray');
+        bw      = rs.getImageData(rFrm, 'bw');
+    end
+    
+    % Extract and set data for contour 
+    ctr = extractContour(bw, sz);    
+    orgn = sprintf('%s_%s_%s_Frm%d', rs.ExperimentName, rs.GenotypeName, rs.getSeedlingName, rFrm);
+    
+    ctr.setGrayImageAtFrame(im, 1);
+    ctr.setBWImageAtFrame(bw, 1);       
+    ctr.setOrigin(orgn);
 
+end
 
 
