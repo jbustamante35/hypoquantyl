4b0dfd10531566c6c2350acf3504b3e9d6cbd3fd
jbustamante35 <jbustamante35@gmail.com>
2018-07-20 16:04:59 -0500

    Normalizing contours to generate probability image, beginning function to predict contour using probability image.
    Former-commit-id: 298cbd15e8adf21250001dce2ea86ab7ecf6a4cf [formerly 0ee0e3e691ac8a9da9ed9da948cec240b6a57c7f [formerly 4bafc6cd9ec22c4114a771a4df5fd9563c6d6c75]]
    Former-commit-id: 0ee0e3e691ac8a9da9ed9da948cec240b6a57c7f
    Former-commit-id: 93b4723ebaac37bef483036aeabcc0fd63ed4654

---
+++ Helpers/classhelp/getDim.m
@@ -0,0 +1,28 @@
+function D = getDim(X, n)
+%% getDim: returns n-dimension of multi-dimensional data
+% This function is basically just a shortcut for concatenating a cell array and returning the
+% desired dimension. It can also be used for a multi-dimensional data matrix.
+%
+% Usage:
+%   D = getDim(X, n)
+%
+% Input:
+%   X: input data as cell array or matrix
+%   n: desired dimension of data to return
+%
+% Output:
+%   D: data from desired dimension defined by n
+%
+
+try
+    if iscell(X)
+        X = cat(1, X{:});
+        D = X(:,n);
+    else
+        D = X(:,n);
+    end
+catch
+    fprint(2, 'Input should either be cell array or data matrix\n');
+end
+
+end
