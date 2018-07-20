43c5d32385acf8221ecf65a9e254e88aae72782c
jbustamante35 <jbustamante35@gmail.com>
2018-07-20 16:04:59 -0500

    Normalizing contours to generate probability image, beginning function to predict contour using probability image.
    Former-commit-id: 298cbd15e8adf21250001dce2ea86ab7ecf6a4cf [formerly 0ee0e3e691ac8a9da9ed9da948cec240b6a57c7f [formerly 4bafc6cd9ec22c4114a771a4df5fd9563c6d6c75]]
    Former-commit-id: 2b2a62dcef0e3d6642b37a5df05020f0d72c1f9d
    Former-commit-id: 8d65345dbee2b57f6e7ca92a47ec1cfe8189bb84
    Former-commit-id: dee853a316cbf2f545e057b86ecf76a49ada6109

---
+++ Helpers/contours/findFrame.m
@@ -0,0 +1,27 @@
+function F = findFrame(S, E)
+%% findFrame: vector representing the rotation needed to change between reference frames
+% This function uses a rotation matrix to compute the 90-degree rotation from the input vector to
+% find it's orthogonal vector. The input and orthogonal vectors define the new reference frame. This
+% function is primarily used for the midpointNorm() function.
+% 
+% Usage:
+%   F = findFrame(S, E)
+%
+% Input:
+%   S: coordinate at start of curve
+%   E: coordinate at end of curve
+%
+% Output:
+%   F: [2 x 2] matrix representing rotated basis vectors
+
+%% Rotation Matrix
+Rmat = @(t) [[cos(t) ; -sin(t)], ...
+    [sin(t) ; cos(t)]];
+R = Rmat(deg2rad(90));
+
+%% New reference frame
+Z = E - S;
+D = Z * norm(Z)^-1;
+N = (R * D')';
+F = [D ; N];
+end
