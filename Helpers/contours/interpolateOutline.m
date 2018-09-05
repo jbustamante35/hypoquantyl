7d0acc0e64413131e6a9d317a79346c9f4a4a660
jbustamante35 <jbustamante35@gmail.com>
2018-09-05 18:08:04 -0500

    Fixed PCA sweeper and ContourJB and various helper files.
    Former-commit-id: 38fe980faa7e89069411d4e81443aba2d574c4ea [formerly e5f40023646930dda1f014e215eea4465bae8924] [formerly ee33195a689f81c28f951116609258796404e7a1 [formerly ee33195a689f81c28f951116609258796404e7a1 [formerly 0811076e019c921b70828d31269714165784eae0]]]
    Former-commit-id: ee33195a689f81c28f951116609258796404e7a1
    Former-commit-id: b7cf62ba4d8c3cdde21b9932211b86a715ad89e6 [formerly b2db2f3c7393860913ad4b4be85ca362e3a94c93]
    Former-commit-id: 3794d3a84add1cb814f4545017bf59c06ac05c0d

--- Helpers/contours/interpolateOutline.m
+++ Helpers/contours/interpolateOutline.m
@@ -1,19 +1,19 @@
 function oL = interpolateOutline(crds, sz)
 %% interpolateOutline: interpolate a set of coordinates to desired length
-% This function takes a set of coordinates, determines cumulative sum of all the arc lengths 
+% This function takes a set of coordinates, determines cumulative sum of all the arc lengths
 % between each coordinate, then interpolates along each axis to the number of coordinates defined by
-% the sz parameter. 
-% 
+% the sz parameter.
+%
 % Usage:
 %   oL = interpolateOutline(crds, sz)
-% 
+%
 % Input:
 %   crds: original set of coordinates to interpolate
 %   sz: length for interpolated set of coordinates
-%   
+%
 % Output:
 %   oL: new set of coordinates of desired length
-% 
+%
 
 %% Arc lengths of countour (distances between points)
 d  = diff(crds, 1, 1);
@@ -24,5 +24,4 @@ L  = cumsum([0 ; dL]);
 oq = linspace(L(1), L(end), sz);
 oL = interp1(L, crds, oq);
 end
-
 
