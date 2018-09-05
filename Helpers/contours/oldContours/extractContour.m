ee33195a689f81c28f951116609258796404e7a1
jbustamante35 <jbustamante35@gmail.com>
2018-09-05 18:08:04 -0500

    Fixed PCA sweeper and ContourJB and various helper files.
    Former-commit-id: 0811076e019c921b70828d31269714165784eae0

--- Helpers/contours/oldContours/extractContour.m
+++ Helpers/contours/oldContours/extractContour.m
@@ -14,30 +14,14 @@ function cntr = extractContour(bw, max_size)
 %
 
 %% Get boundaries of inputted bw image
-bndAll = bwboundaries(bw, 'noholes');
+bndAll   = bwboundaries(bw, 'noholes');
 [~, lrg] = max(cellfun(@numel, bndAll));
-bnds = bndAll{lrg};
+bnds     = bndAll{lrg};
 %     bnds = bndAll{1};
 
 %% Interpolate distances to an equalized number of coordinates
+bnds   = [getDim(bnds, 2) , getDim(bnds, 1)]; % Switch y-/x-coordinates to x-/y-coordinates
 intrps = interpolateOutline(bnds, max_size);
-
-% %% Arc lengths of countour (distances between points)
-% d  = diff(bnds, 1, 1);
-% dL = sum(d .* d, 2) .^ 0.5;
-% 
-% %% Interpolate distances to an equalized number of coordinates
-% L = cumsum([0 ; dL]);
-% 
-% % Interpolate along x-coordinates
-% xv     = bnds(:,1);
-% xq     = linspace(L(1), L(end), max_size);
-% I(:,1) = interp1(L, xv, xq);
-% 
-% % Interpolate along y-coordinates
-% yv     = bnds(:,2);
-% yq     = linspace(L(1), L(end), max_size);
-% I(:,2) = interp1(L, yv, yq);
 
 %% Output final structure
 cntr = ContourJB('Outline', bnds, 'InterpOutline', intrps);
