8a3c1f7e81c2bfb1e9a8fb6f14e8f285213de515
jbustamante35 <jbustamante35@gmail.com>
2018-09-21 10:39:43 -0500

    Updates before renaming directory names
    Former-commit-id: 4fa18497fb38daaace2ae4b0f306961d5189053a [formerly 14547e92c40ef64d0e415f23e553494b6ad86a3b [formerly bd113623f79723d68ff85dd8b1fc5369290fc665] [formerly 4fa18497fb38daaace2ae4b0f306961d5189053a [formerly 2597528eb44f34cecc308abe6914640420b9910a]]]
    Former-commit-id: 091cf5e763b8ac8810eb3d97a26c079dbcee289c
    Former-commit-id: be94733ddfb6ec7a2715ddb8cc2d9c575c6eba03
    Former-commit-id: ad24ec14c9c23184b1ad803c608227caa1cba43a [formerly a3bc38473a231e465416d2d28d5659527f408609]
    Former-commit-id: 5a7b7c51cf6dbe9d83862110f1f08f6939c738fa

--- Helpers/classhelp/segSmooth.m
+++ Helpers/classhelp/segSmooth.m
@@ -1,8 +1,9 @@
-function smth = segSmooth(seg, span)
+function smth = segSmooth(seg, span, method)
 %% segSmooth: Smooth curve of given type defined by req parameter
 % Curve is smoothed by moving average span from SMOOTHSPAN constant
 try
-    smth = reshape(smooth(seg, span), size(seg));
+    y    = smooth(seg(:,1), seg(:,2), span, method);
+    smth = [seg(:,1) y];
 catch
     fprintf(2, 'Error smoothing segment\n');
 end
