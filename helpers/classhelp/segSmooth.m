c42cba6cc419ed585f6066d83e7b28bc9aaed405
Julian Bustamante <jbustamante35@gmail.com>
2018-09-27 03:57:07 -0500

    Created jdev branch. Smoothing algorithm for midpoint-normalized Curve segments

--- helpers/classhelp/segSmooth.m
+++ helpers/classhelp/segSmooth.m
@@ -2,10 +2,13 @@ function smth = segSmooth(seg, span, method)
 %% segSmooth: Smooth curve of given type defined by req parameter
 % Curve is smoothed by moving average span from SMOOTHSPAN constant
 try
-    y    = smooth(seg(:,1), seg(:,2), span, method);
-    smth = [seg(:,1) y];
+    seg_in = seg(2:(end-1), :);
+    y_out  = smooth(seg_in(:,1), seg_in(:,2), span, method);
+    yf     = [seg_in(1,2) ; y_out ; seg_in(end,2)];
+    smth   = [seg(:,1) yf];
 catch
     fprintf(2, 'Error smoothing segment\n');
-end
+    smth = [];
+ends
 
 end
