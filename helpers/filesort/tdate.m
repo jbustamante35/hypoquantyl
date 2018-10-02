b6b37947d692ec84407a84d7a5bb241472044cbc
Julian Bustamante <jbustamante35@gmail.com>
2018-10-01 21:19:18 -0500

    Updated directory names. Started refactoring main classes.

--- helpers/filesort/tdate.m
+++ helpers/filesort/tdate.m
@@ -1,6 +1,21 @@
-function dt = tdate
+function dt = tdate(varargin)
 %% tdate: shortcut function to get today's date string in the format that I use a lot
+% I don't get why nargin being true (there are indeed no arguments) evaluates to 0 (\facepalm)
 
-dt = datestr(now, 'yymmdd');
+if ~nargin
+    dt = datestr(now, 'yymmdd');
+else
+    ver = varargin{1};
+    switch ver
+        case 's'
+            dt = datestr(now, 'yymmdd');
+        case 'm'
+            dt = datestr(now, 1);
+        case 'l'
+            dt = datestr(now, 'ddd-mmm-dd-yyyy-HHMM');
+        otherwise
+            dt = datestr(now, 'yymmdd');
+    end
+end
 
 end
