285323869c0666ff683a92a4f1da68d895dfff2a
Julian Bustamante <jbustamante35@gmail.com>
2018-10-23 15:29:29 -0500

    Refactoring to change writing image to memory to reading from path name
    Former-commit-id: 28e73a0d6a0e477b385915568ded5acee3d89060 [formerly f6374a77656ca8a418bb97146e6acae84c5e874a [formerly f454da8a4046b782035fd4694397af82f7e90ecd]]
    Former-commit-id: 8b4d6e1789df78e8347a7f4bb0fb921e794038cc
    Former-commit-id: 65c6d4d26e55443f772d0fa734bb3431f023d609
    Former-commit-id: 45d6b061df03e7daad6f397e555a16c5ac9af513

--- helpers/classhelp/cropFromAnchorPoints.m
+++ helpers/classhelp/cropFromAnchorPoints.m
@@ -1,10 +1,10 @@
-function img_out = cropFromAnchorPoints(img_input, anchor_points, scale_size)
+function [img_out, bbox] = cropFromAnchorPoints(img_input, anchor_points, scale_size)
 %% cropFromAnchorPoints: crop out and rescale an image using specfic anchorpoints
 % This function is used by Seedling.FindHypocotyl to use defined anchorpoints to determine where to
 % crop out the sub-image. The image is cropped and then rescaled to size defined by scale_size.
 %
 %  Usage:
-%      [output1, output2] = functionName(input1, input2)
+%      [img_out, bbox] = cropFromAnchorPoints(img_input, anchor_points, scale_size)
 %
 %  Input:
 %      img_input: original image to crop and resize
@@ -15,10 +15,9 @@ function img_out = cropFromAnchorPoints(img_input, anchor_points, scale_size)
 %      img_out: cropped and rescaled image
 %
 
-
-%% Wow this is so complicated
-crp1    = [0 0 anchor_points(4,1) anchor_points(2,2)]; % Crop region containing PreHypocotyl
-cim1    = imcrop(img_input, crp1);                     % Crop image of PreHypocotyl
+%% Create crop box --> Crop image --> Rescale cropped image
+bbox    = [0 0 anchor_points(4,1) anchor_points(2,2)]; % Crop region containing PreHypocotyl
+cim1    = imcrop(img_input, bbox);                     % Crop image of PreHypocotyl
 img_out = imresize(cim1, scale_size);                  % Final rescaled image
 
 end
