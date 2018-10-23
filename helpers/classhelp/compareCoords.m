0ed95920034ab3ccdbc722b0de63152b3fc39d9f
Julian Bustamante <jbustamante35@gmail.com>
2018-10-23 15:29:29 -0500

    Refactoring to change writing image to memory to reading from path name
    Former-commit-id: 822aec0b282f20f1a996b41ff9671404df2fd9c0 [formerly 28e73a0d6a0e477b385915568ded5acee3d89060] [formerly f6374a77656ca8a418bb97146e6acae84c5e874a [formerly f6374a77656ca8a418bb97146e6acae84c5e874a [formerly f454da8a4046b782035fd4694397af82f7e90ecd]]]
    Former-commit-id: f6374a77656ca8a418bb97146e6acae84c5e874a
    Former-commit-id: 562670c1b32c18c93c370eb242b145cf61950a13 [formerly 0731928c2a3aee3e25fd9d18c8d64a09e318aed9]
    Former-commit-id: 47e6f2f96605dfdebf94fc7e158c22e8bdb79d2c

--- helpers/classhelp/compareCoords.m
+++ helpers/classhelp/compareCoords.m
@@ -1,19 +1,19 @@
 function minDistIdx = compareCoords(crdsIn,  crdsNxt, dim)
-%% Compare inputted coordinates with group of coordinates
+%% Euclidean distance comparison of inputted coordinates with group of coordinates
 % If coordinates are within set error percent, it is added to the
 % next frame of the Seedling. Otherwise the frame is skipped.
 %
 % Input:
-%   crdsIn: coordinates of previous frame for current Seedling
-%   crdsNxt: cell array of Seedlings at specific frame to compare to pf
-%   d : Euclidean distances between pf and cf
+%   crdsIn: [n x m] vector of unmatched coordinates 
+%   crdsNxt: [n x m] vector of coordinates to compare to crdsIn
+%   d : Euclidean distances between crdsIn and crdsNxt
 %
 % Output:
 %   minDistIdx: index where each crdsIn coordinate is closest in crdsNxt
 
 minDistIdx = nan(length(crdsIn), 1);
-for i = 1 : length(crdsIn)
-    [~, y] = min(abs(bsxfun(@minus, crdsNxt, crdsIn(i,:))));
+for i = 1 : numel(minDistIdx)
+    [~, y]        = min(abs(bsxfun(@minus, crdsNxt, crdsIn(i,:))));
     minDistIdx(i) = y(dim);
 end
 
