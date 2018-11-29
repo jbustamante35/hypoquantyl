4d95af0b797203495a2594995c9d5418d10d6151
Julian Bustamante <jbustamante35@gmail.com>
2018-11-29 00:06:48 -0600

    Homebase finished fixing randomCircuits method.
    Former-commit-id: 88112a1ac318b4f7510ab48381360399ace88813 [formerly f80b0911d2c39e38f10e731a6400121d9043d0ca] [formerly 2431d5102a62b3c10f3488c27c092ee049d86998 [formerly 2431d5102a62b3c10f3488c27c092ee049d86998 [formerly 907c9786455a273ccbbb1ed3946d1bf1403ea7ac]]]
    Former-commit-id: 2431d5102a62b3c10f3488c27c092ee049d86998
    Former-commit-id: b93b874c4247cb07c9fc665bc587232d9988455c [formerly a80e3ddffcc3d41a530d9c466c501d0a97c31172]
    Former-commit-id: 1a4e8f2afe8606ebb36fdee023c52a7cc45ae0b3

--- classes/Experiment.m
+++ classes/Experiment.m
@@ -9,13 +9,13 @@ classdef Experiment < handle
         ExperimentPath
         NumberOfGenotypes
     end
-    
+
     properties (Access = private)
         %% Private data properties
         Genotypes
         HYPOCOTYLLENGTH = 250 % Distance to set cutoff for hypocotyl
     end
-    
+
     methods (Access = public)
         %% Constructor and main methods
         function obj = Experiment(varargin)
@@ -23,27 +23,27 @@ classdef Experiment < handle
             if ~isempty(varargin)
                 % Parse inputs to set properties
                 args = obj.parseConstructorInput(varargin);
-                
+
                 fn = fieldnames(args);
                 for k = fn'
                     obj.(cell2mat(k)) = args.(cell2mat(k));
                 end
-                
+
             else
                 % Set default properties for empty object
                 obj.ExperimentPath = pwd;
             end
-            
+
             [obj.ExperimentName, ~] = getDirName(obj.ExperimentPath);
-            
+
         end
-        
+
         function obj = AddGenotypes(obj)
             %% Get all directories (exclude . and ..)
             fld   = dir(obj.ExperimentPath);
             fld   = fld(3:end);
             fldrs = fld(cat(1, fld.isdir));
-            
+
             for f = fldrs'
                 try
                     obj.Genotypes(obj.NumberOfGenotypes + 1) = ...
@@ -55,11 +55,11 @@ classdef Experiment < handle
                     	obj.NumberOfGenotypes);
                 end
             end
-            
-        end        
-        
+
+        end
+
         function obj = FindHypocotylAllGenotypes(obj, v)
-            %% Extract Hypocotyl from every frame of each Seedling from 
+            %% Extract Hypocotyl from every frame of each Seedling from
             % all Genotypes from this Experiment object
             try
 
@@ -68,8 +68,8 @@ classdef Experiment < handle
             	    	obj.ExperimentName);
             	    tic;
             	end
-            
-                gens  = obj.Genotypes;
+
+                gens = obj.Genotypes;
                 arrayfun(@(x) x.FindHypocotylAllSeedlings(v), ...
                 	gens, 'UniformOutput', 0);
 
@@ -91,7 +91,7 @@ classdef Experiment < handle
         function obj = FindSeedlingAllGenotypes(obj, v)
             %% Extract Seedling from every frame of each Genotype
 
-            try 
+            try
                 if v
                     fprintf('Extracting Seedlings from Experiment %s\n', ...
                     	obj.ExperimentName);
@@ -119,7 +119,7 @@ classdef Experiment < handle
         end
 
       function obj = SaveExperiment(obj)
-         %% Prune superfluous data, dereference parents, then save 
+         %% Prune superfluous data, dereference parents, then save
          % Remove RawSeedlings and PreHypocotyls
              g = obj.combineGenotypes;
              arrayfun(@(x) x.PruneSeedlings, g, 'UniformOutput', 0);
@@ -145,17 +145,17 @@ classdef Experiment < handle
 
          s = obj.combineSeedlings;
          h = obj.combineHypocotyls;
-         
+
          X = {g, s, h};
          ref = @(c) arrayfun(@(x) x.RefChild, c, 'UniformOutput', 0);
          cellfun(@(x) ref(x), X, 'UniformOutput', 0);
      end
 
     end
-    
+
     methods (Access = public)
         %% Various helper methods for other classes to use
-        
+
         function g = getGenotype(obj, idx)
             %% Returns genotype at desired index
             try
@@ -165,16 +165,16 @@ classdef Experiment < handle
                 fprintf(2, '%s \n', e.message);
             end
         end
-        
+
         function G = combineGenotypes(obj)
             %% Combine all Genotypes into single object array
             G = obj.getGenotype(':');
         end
-        
+
         function g = search4Genotype(obj, nm)
             %% Return specific Genotype by GenotypeName
             gts = obj.getGenotype(':')';
-            
+
             for gt = gts
                 mtc = gt.GenotypeName;
                 if isequal(nm, mtc)
@@ -183,19 +183,19 @@ classdef Experiment < handle
                 end
             end
         end
-        
+
         function S = combineSeedlings(obj)
             %% Combine all Seedlings into single object array
             G = obj.combineGenotypes;
             S = arrayfun(@(x) x.getSeedling(':'), G, 'UniformOutput', 0);
             S = cat(1, S{:});
         end
-        
+
         function H = combineHypocotyls(obj, req)
             %% Combine all Hypocotyls into single object array
             % CHANGE THIS WHEN I FIX HOW HYPOCOTYLS ARE STORED IN SEEDLINGS
-            % Each Seedling will have a single Hypocotyl, derived from data 
-            % from the combination of good frames of each PreHypocotyl. 
+            % Each Seedling will have a single Hypocotyl, derived from data
+            % from the combination of good frames of each PreHypocotyl.
             S = obj.combineSeedlings;
 
             try
@@ -216,7 +216,7 @@ classdef Experiment < handle
             end
 
         end
-        
+
         function C = combineContours(obj)
             %% Return all Hypocotyls with manually-drawn CircuitJB objects
             H   = obj.combineHypocotyls;
@@ -226,26 +226,26 @@ classdef Experiment < handle
             flp = cat(1, flp{:});
             C   = [org ; flp];
         end
-        
+
         function P = combineParameters(obj)
-            %% Return all Ppar (theta, dX, dY) from CircuitJB Routes 
+            %% Return all Ppar (theta, dX, dY) from CircuitJB Routes
             C = obj.combineContours;
             P = arrayfun(@(x) x.ParameterRoutes, C, 'UniformOutput', 0);
             P = cat(3, P{:});
             P = permute(P, [3 2 1]);
         end
-        
+
         function check_outOfFrame(obj, frm, s)
             %% Check if Seedling grows out of frame
             %
             % obj: this Experiment
             % frm: frame of time-lapse
             % s  : Seedling to check
-            
+
         end
-        
+
     end
-    
+
     methods (Access = private)
         %% Private helper methods for this class
         function args = parseConstructorInput(varargin)
@@ -256,12 +256,12 @@ classdef Experiment < handle
             p.addOptional('ExperimentName', '');
             p.addOptional('NumberOfGenotypes', 0);
             p.addOptional('Genotypes', Genotype);
-            
+
             % Parse arguments and output into structure
             p.parse(varargin{2}{:});
             args = p.Results;
         end
-        
-        
+
+
     end
 end
