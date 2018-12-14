b32d892f58ef3ad1ff6f81f4ee682f6bb2136015
Julian Bustamante <jbustamante35@gmail.com>
2018-12-13 19:33:04 -0600

    Fixed methods for converting envelope coordinates to image coordinates
    Former-commit-id: 3ec20587a4e27ed9aa5dbcac4ab74e7f9bea8cb7 [formerly ae6ac1b08f6d991c4c5fff764830c0b7f6d3f81b [formerly 5f077e914c2bda70183b1311ffc7d219371ccea1]]
    Former-commit-id: ae6ac1b08f6d991c4c5fff764830c0b7f6d3f81b
    Former-commit-id: 51d0d95c2b0e40269a5ec832866e4daca4cc219b

--- plots/check_pca/randomContourAndSegment.m
+++ plots/check_pca/randomContourAndSegment.m
@@ -11,10 +11,15 @@ function [cIdx, cntr, crvs, segs, sIdx] = randomContourAndSegment(cntrs, rndFun)
 %   [cIdx, cntr, crvs, segs, sIdx] = randomContourAndSegment(cntrs, rndFun)
 %
 % Input:
-%
+%   cntrs: object array of CircuitJB objects to draw from
+%   rndFun: function handle to draw from a random selection
 %
 % Output:
-%
+%   cIdx: index of CircuitJB object taken from array
+%   cntr: handle to chosen CircuitJB object
+%   crvs: handle to child Curve object from chosen cntr
+%   segs: total number of segment around cntr
+%   sIdx: index of segment chosen from crvs
 %
 
 if ~isa(rndFun, 'function_handle')
