c191833d1450b464b17ccfcbec98359246947be6
Julian Bustamante <jbustamante35@gmail.com>
2019-01-23 17:21:45 -0600

    Add save feature for collectTrainingSet, error catch for figure save for randomCircuits
    Former-commit-id: c207d9b11a9c17ade7b78c1ba7c5b196cb3dcd1a [formerly efe5d748fee6965c5083814221e35d27e694525d [formerly 92c8f9bea655aafe4aec8786690a3506a022f9e5]]
    Former-commit-id: efe5d748fee6965c5083814221e35d27e694525d
    Former-commit-id: a932e282c60ab3fa3d4dc7ef500a57121165112d

--- helpers/training/collectTrainingSet.m
+++ helpers/training/collectTrainingSet.m
@@ -1,4 +1,4 @@
-function [T, A] = collectTrainingSet(crvs)
+function [T, Z] = collectTrainingSet(crvs, sv)
 %% collectTrainingSet: compile data structures for training set
 % This function takes in an array of CircuitJB objects with their child Curve
 % objects and combines them to generate the dataset to use for training. This is
@@ -17,9 +17,11 @@ function [T, A] = collectTrainingSet(crvs)
 %
 % Input:
 %   crvs: array of Curve objects made from manually-drawn contours
+%   sv: boolean to save data in a .mat file
 %
 % Ouput:
 %   T: structure containing all training data (see above for details)
+%   Z: structure containing reshaped and vectorized training data
 %
 
 %% Create output structure and collect data
@@ -73,8 +75,14 @@ rastTngt = cat(1, T.rTngt{:}) + rastMids;
 rastNorm = cat(1, T.rNorm{:}) + rastMids;
 rastZvec = [rastMids , rastTngt , rastNorm];
 
-%% Store reshaped data into A structure
-A = struct('xCrds', rastCrds{1}, 'yCrds', rastCrds{2}, 'zVect', rastZvec);
+% Store reshaped data into Z structure
+Z = struct('xCrds', rastCrds{1}, 'yCrds', rastCrds{2}, 'zVect', rastZvec);
+
+%% Save data into .mat file
+if sv
+    fnm = sprintf('%s_TrainingData_%dContours', tdate('s'), numel(T.rCrds));
+    save(fnm, 'T', 'Z');
+end
 
 end
 
