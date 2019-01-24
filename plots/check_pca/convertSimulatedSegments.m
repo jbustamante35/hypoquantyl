b0b0a489c6d83f8529ee189d825742f95895a547
Julian Bustamante <jbustamante35@gmail.com>
2019-01-24 16:37:37 -0600

    Implemented fully interpolated and wrapped contours, fixed envelope method, temporarily omitting patches
    Former-commit-id: 168479ccafab5f4aeb8e73ada768f0816524d50e [formerly 7d3b1c1cfebda798e300c29e95c7147f9382fd75 [formerly 5ddf894117d54d8de7552086d0c599cf11c2ed6a]]
    Former-commit-id: 7d3b1c1cfebda798e300c29e95c7147f9382fd75
    Former-commit-id: 86c473915eb29eeaac9a474b7f1dac99fd0e5e8b

--- plots/check_pca/convertSimulatedSegments.m
+++ plots/check_pca/convertSimulatedSegments.m
@@ -1,10 +1,18 @@
 function [raw, cnv] = convertSimulatedSegments(truX, truY, simX, simY, crv)
-%% convertSimulatedSegments: convert simulated data from PCA to raw segment coordinates
-% Input:
+%% convertSimulatedSegments: convert simulated data to raw segment coordinates
+% This takes a single set of coordinates and uses the inverse normalization
+% method to convert midpoint-normalized coordinates to raw image coordinates.
 %
+% Input:
+%   truX: x-coordinates used as input for PCA
+%   truY: y-coordinates used as input for PCA
+%   simX: simulated x-coordinates after PCA analysis
+%   simY: simulated y-coordinates after PCA analysis
+%   crv: Curve object to extract P-matrix and midpoint data
 %
 % Output:
-%
+%   raw: converted input coordinates in image reference frame
+%   cnv: converted simulated coordinates in image reference frame
 %
 
 % Get Pmat and MidPoint parameters for reverse midpoint-normalization
@@ -12,8 +20,11 @@ pmL = crv.getParameter('Pmats', ':');
 mdL = crv.getMidPoint(':');
 
 % Convert midpoint-normalized PCA data to raw segment coordinates
-midNorm = @(Dx, Dy, Pm, md, n) reverseMidpointNorm([Dx(n,:) ; Dy(n,:)]', Pm(:,:,n)) + md(n,:);
-raw = arrayfun(@(x) midNorm(truX, truY, pmL, mdL, x), 1:size(truX,1), 'UniformOutput', 0);
-cnv = arrayfun(@(x) midNorm(simX, simY, pmL, mdL, x), 1:size(simX,1), 'UniformOutput', 0);
+midNorm = @(Dx, Dy, Pm, md, n) reverseMidpointNorm([Dx(n,:) ; Dy(n,:)]', ...
+    Pm(:,:,n)) + md(n,:);
+raw = arrayfun(@(x) midNorm(truX, truY, pmL, mdL, x), ...
+    1:size(truX,1), 'UniformOutput', 0);
+cnv = arrayfun(@(x) midNorm(simX, simY, pmL, mdL, x), ...
+    1:size(simX,1), 'UniformOutput', 0);
 
 end
