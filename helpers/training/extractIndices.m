0000388cde6ba0e3cea9378e732b6dc584c711d5
Julian Bustamante <jbustamante35@gmail.com>
2019-01-29 17:37:59 -0600

    Switched back to impixel for mapCurve2Image, extractIndicies added, tweaks to obtaining training set
    Former-commit-id: 53fe03d2419ceae11d74454ce0e4ad97e1f3f8c1 [formerly be30030a3e9b3909d9000f5bd64731bda55a5a15 [formerly 8f6733d6518d40d848207acf9e290a9a2c35a156]]
    Former-commit-id: be30030a3e9b3909d9000f5bd64731bda55a5a15
    Former-commit-id: 4b149ffaf48ce78392eb9b13b066c4d3012adc10

---
+++ helpers/training/extractIndices.m
@@ -0,0 +1,29 @@
+function [I, D] = extractIndices(idx, ttlSegs, spltData)
+%% extractIndicies: extract indices of contour by splitting by segments
+% This function
+%
+% Usage:
+%   [I, D] = extractIndices(idx, ttlSegs, splitData)
+%
+% Input:
+%   idx: index to extract coordinates from
+%   ttlSegs: total number of segments to split full dataset by
+%   spltData: data to actually split dateset, set to 0 to return only indicies
+%
+% Output:
+%   I: array of indices corresponding to idx
+%   D: subset of splitData corresponding to indices from I
+%
+
+splitIdx = @(c,s) (((s * c) - s) + 1) : ((s * (c+1)) - s);
+getIdx   = @(x)   splitIdx(x, ttlSegs);
+
+I = getIdx(idx);
+
+if ~isempty(spltData)
+    D = spltData(I,:)';
+else
+    D = [];
+end
+
+end
