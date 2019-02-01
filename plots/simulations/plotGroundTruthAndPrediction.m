4babeac736867104ff20e5cc3969a3c71b739496
Julian Bustamante <jbustamante35@gmail.com>
2019-01-31 21:54:46 -0600

    added plotter for comparing ground truth and predictions
    Former-commit-id: 9f6284796a8187a3f0b9440895535f349b91303f [formerly c9e0cdaff4d5b72cc33e3a0978b8c0704005c873 [formerly 0b4f6656a18ee52e24710761f07c8af9af0f0c2e] [formerly 9369dd587098bcc16de9bc98a15fa88e9119c6b8 [formerly 325228cce2c409a3a80fb99e944e3ea4410aadfb]] [formerly 9f6284796a8187a3f0b9440895535f349b91303f [formerly 325228cce2c409a3a80fb99e944e3ea4410aadfb [formerly 637187046fa3b9e69cc213be5a8cb10943c12e53]]]]
    Former-commit-id: dfe0cf0a91edb689f04b736961f6d3c08cdf9c1c
    Former-commit-id: 622909c7f3c89df3ccc7fa707047823d1a5e0c73
    Former-commit-id: 4d3eaca0be698dcfe9cf704015c7f14a6f5fa148 [formerly ebb76991209411706ceb6580795189abc0faafb3 [formerly f336eb39fe2630466bc3b44432bb28f1fc1e739e]]
    Former-commit-id: 1e0402e9891a7716926b500df8ec43b300fec2d0 [formerly ebb76991209411706ceb6580795189abc0faafb3]
    Former-commit-id: aa30b4791b644a4481d60b763138cbdbb50882cf

---
+++ plots/simulations/plotGroundTruthAndPrediction.m
@@ -0,0 +1,46 @@
+function fig = plotGroundTruthAndPrediction(X, Y, I, idx, numPCs, sav, f)
+%% plotGroundTruthAndPrediction: overlay ground truth contour on predicted
+%
+%
+% Usage:
+%   fig = plotGroundTruthAndPrediction(truX, preY, img, idx, numComponents, f)
+%
+% Input:
+%   X: ground truth contour coordinates
+%   Y: predicted contour coordinates
+%   I: grayscale image associated with contour
+%   idx: index in Curve object array for naming figure
+%   numPCs: [4 x 1] array of principal component values used for prediction
+%   sav: boolean to save figure as .fig and .tiff image
+%   f: index of figure handle to plot data onto
+%
+% Output:
+%   fig: handle to outputted figure
+%
+
+% Plot predictions and truths
+fig = figure(f);
+cla;clf;
+
+% Show image with ground truth and simulated prediction
+imshow(I, []);
+hold on;
+plt(X, 'g--' , 3);
+plt(Y, 'y-' , 3);
+ttl = ...
+    sprintf('Contour Prediction\nTruth (green) | Predicted (yellow)\nContour %d', ...
+    idx);
+title(ttl);
+
+% Save figure
+if sav
+    
+    fnm = sprintf('%s_ContourPrediction_p%dPCs_m%dPCs_x%dPCs_y%dPCs_Contour%d', ...
+        tdate('s'), numPCs(1), numPCs(2), numPCs(3), numPCs(4), idx);
+    savefig(fig, fnm);
+    saveas(fig, fnm, 'tiffn');
+else
+    pause(0.5);
+end
+
+end
