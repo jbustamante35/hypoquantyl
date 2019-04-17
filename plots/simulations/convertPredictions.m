b6efed486d39231f69bd8546fe9f31be0f902670
Julian Bustamante <jbustamante35@gmail.com>
2019-04-17 12:55:45 -0500

    jdev updates 04-17, batch conversions from cnn to raw formats
    Former-commit-id: 40451a68e9f40130ca1c82af6af387183a75e5be [formerly 8dd6c08bd7f0849a0805d0d4ac74876434a038fc]
    Former-commit-id: 215d1778dc766caba46ee0352bbb78c3072b80bf

--- plots/simulations/convertPredictions.m
+++ plots/simulations/convertPredictions.m
@@ -1,4 +1,4 @@
-function [segInp_truth, segSim_pred, figs] = convertPredictions(idx, D, predZ, predMethod, px, py, pz, sav)
+function Zn = convertPredictions(predZ, req, px, py, pz, sav)
 %% convertPredictions:
 % This
 %
@@ -12,46 +12,119 @@ function [segInp_truth, segSim_pred, figs] = convertPredictions(idx, D, predZ, p
 %
 %
 
-%% Set figure indices [recommended to have 3 figures already opened!]
-figs(1) = figure(1);
-figs(2) = figure(2);
-figs(3) = figure(3);
-
-%% Extract some technical data
-pcx = length(px.EigValues);
-pcy = length(py.EigValues);
-pcz = length(pz.EigValues);
-%     pcr = size(predZ); % For PLSR output
-
-%%
-switch predMethod
-    case 'plsr'
-        % TODO convert PLSR output to original Z-Vector
-        cnvMethod = @(x) x;
-        
-    case 'cnn'
-        % Use function to revert CNN output to original Z-Vector
-        numCrvs   = size(predZ, 1);
-        ttlSegs   = size(predZ, 2) / 6;
-        cnvMethod = @(x) zVectorConversion(x, ttlSegs, numCrvs, 'rev');
+%% Get full Z-Vector data set
+switch req
+    case 'truth'
+        Xdat = pz.InputData;
+        dat  = 'InputData';
+    case 'sim'
+        Xdat = pz.SimData;
+        dat  = 'SimData';
+    case 'predicted'
+        Xdat = predZ;
+        dat  = 'SimData';
+    otherwise
+        % Default to ground truth
+        Xdat = pz.InputData;
+        dat  = 'InputData';
 end
 
-%% Plot Frankencotyls to show backbone predictions [single]
-tmpZ = cnvMethod(predZ);
+%% Extract set-up data
+ttlSegs = size(predZ, 2) / 6;
+numCrvs = size(predZ, 1);
+sIdxs   = 1 : ttlSegs;
+cIdxs   = 1 : numCrvs;
+lngSegs = size(px.(dat),2);
+halfIdx = ceil(lngSegs / 2);
 
-% Show Ground Truth
-[segInp_truth, ~] = ...
-    plotPredictions(idx, px, py, pz, tmpZ, D, 'truth', sav, 1);
+%% Revert output back to rasterized form
+Xrev = zVectorConversion(Xdat, ttlSegs, numCrvs, 'rev');
+Xns  = arrayfun(@(x) extractIndices(x, ttlSegs, Xrev)', ...
+    cIdxs, 'UniformOutput', 0);
 
-% Show Predicted
-[~, segSim_pred] = ...
-    plotPredictions(idx, px, py, pz, tmpZ, D, 'predicted', sav, 2);
+%% Regenerate Pmat from Z-Vector
+Pms_cell = cellfun(@(x) arrayfun(@(y) reconstructPmat(x(y,:)), ...
+    1:length(x), 'UniformOutput', 0), ...
+    Xns, 'UniformOutput', 0);
 
-%% Plot predictions and truths
-chkX = segInp_truth;
-chkY = segSim_pred;
-I    = D(idx).Parent.getImage('gray');
-P    = [pcx, pcy, pcz];
-plotGroundTruthAndPrediction(chkX, chkY, I, idx, P, sav, 3);
+Pm = zeros(3, 3, ttlSegs, numCrvs);
+for p = 1:numel(Pms_cell)
+    Pms = Pms_cell{p};
+    Pm(:,:,:,p) = cat(3, Pms{:});
+end
+
+%% Extract each segment's midpoint-normalized x-/y-coordinates
+nxis = arrayfun(@(x) extractIndices(x, ttlSegs, px.(dat))', ...
+    cIdxs, 'UniformOutput', 0);
+nyis = arrayfun(@(x) extractIndices(x, ttlSegs, py.(dat))', ...
+    cIdxs, 'UniformOutput', 0);
+
+%% Combine midpoint-normalized x-/y-coordinates into single cell array
+xis = cellfun(@(x) arrayfun(@(y) x(y,:)', sIdxs, 'UniformOutput', 0), ...
+    nxis, 'UniformOutput', 0);
+yis = cellfun(@(x) arrayfun(@(y) x(y,:)', sIdxs, 'UniformOutput', 0), ...
+    nyis, 'UniformOutput', 0);
+nis = cellfun(@(xi,yi) cellfun(@(x,y) [x y], xi, yi, 'UniformOutput', 0), ...
+    xis, yis, 'UniformOutput', 0);
+
+%% Convert to image coordinates using processed Z-Vectors
+[ci , ni] = deal(zeros(lngSegs, 2, ttlSegs, numCrvs));
+hi        = zeros(1, 2, ttlSegs, numCrvs);
+for c = cIdxs
+    Ni = nis{c};
+    mp = Xns{c};
+    for s = sIdxs
+        pm          = Pm(:,:,s,c);
+        ci(:,:,s,c) = reverseMidpointNorm(Ni{s}, pm) + mp(s,1:2);
+        ni(:,:,s,c) = Ni{s};
+        
+        % Get half coordinates of segments [minimize amount of data to plot]
+        hi(:,:,s,c) = ci(halfIdx,:,s,c);
+    end
+end
+
+%% Store output in structure
+Zn = struct('FullData', Xdat, 'RevertData', Xrev, 'NormalData', ni, ...
+    'ConvertData', ci, 'Pmat', Pm, 'HalfData', hi);
+
+%% Revert output back to rasterized form
+% Xrev = zVectorConversion(Xdat, ttlSegs, numCrvs, 'rev');
+% Xn   = extractIndices(idx, ttlSegs, Xrev)';
+%
+% % Regenerate Pmat from Z-Vector
+% Pm = arrayfun(@(x) reconstructPmat(Xn(x,:)), 1:length(Xn), 'UniformOutput', 0);
+% Pm = cat(3, Pm{:});
+%
+% % Extract each segment's midpoint-normaized x-/y-coordinates
+% nxi = extractIndices(idx, ttlSegs, px.(dat))';
+% nyi = extractIndices(idx, ttlSegs, py.(dat))';
+%
+% % Combine x-/y-coordinates into single cell array
+% sIdxs = 1 : ttlSegs;
+% xi    = arrayfun(@(x) nxi(x,:), sIdxs, 'UniformOutput', 0);
+% yi    = arrayfun(@(x) nyi(x,:), sIdxs, 'UniformOutput', 0);
+% ni    = arrayfun(@(x) [xi{x}' yi{x}'], sIdxs, 'UniformOutput', 0);
+%
+% % Convert to image coordinates using processed Z-Vectors
+% cp = arrayfun(@(x) reverseMidpointNorm(ni{x}, Pm(:,:,x)) + Xn(x,1:2), ...
+%     sIdxs, 'UniformOutput', 0);
+%
+% % Get half coordinates of segments [minimize amount of data to plot]
+% halfIdx = ceil(size(nxi,2) / 2);
+% hp   = cellfun(@(x) x(halfIdx,:), cp, 'UniformOutput', 0);
+% hp   = cat(1, hp{:});
+%
+% % Store output in structure
+% Zn = struct('FullData', Xdat, 'RevertData', Xrev, 'Pmat', Pm, 'HalfData', hp, ...
+%     'M', Xn(:,1:2), 'T', Xn(:,3:4), 'N', Xn(:,5:6));
+
+%% Save data
+if sav
+    pcx = length(px.EigValues);
+    pcy = length(py.EigValues);
+    pcz = length(pz.EigValues);
+    fn = sprintf('%s_ConvertedZvector_x%d_y%d_z%d', tdate('s'), pcx, pcy, pcz);
+    save(fn, '-v7.3', 'Zn');
+end
 
 end
--- plots/simulations/plotPredictions.m
+++ plots/simulations/plotPredictions.m
@@ -21,94 +21,101 @@ function [syntHalf_inp, syntHalf_sim, fig] = plotPredictions(idx, px, py, pz, pM
 %   fig: figure handle to generated data
 %
 
-%% Set-up figure
-fig = figure(f);
-set(0, 'CurrentFigure', figure(f));
-set(figure(f), 'Color', 'w');
+%% Plot Contours to Compare Ground Truth vs Predicted
+fIdx = f;
+set(0, 'CurrentFigure', figs(fIdx));
 cla;clf;
 
-%% Determine number of PCs and run through plotter
-pcx  = length(px.EigValues);
-pcy  = length(py.EigValues);
-pcz  = length(pz.EigValues);
+% Figure data
+row  = 2;
+col  = 2;
+pIdx = 1;
 
-%% 
-%% Get midpoint indices and image data for contour
-crv        = crvs(idx);
-img        = crv.Parent.getImage('gray');
-mids_truth = crv.getMidPoint(1:crv.NumberOfSegments);
-
-% Extract x-/y-coordinates of contour from input and simulated PCA data
-ttlSegs  = crv.NumberOfSegments;
-[~, nxi] = extractIndices(idx, ttlSegs, px.InputData);
-[~, nxs] = extractIndices(idx, ttlSegs, px.SimData);
-[~, nyi] = extractIndices(idx, ttlSegs, py.InputData);
-[~, nys] = extractIndices(idx, ttlSegs, py.SimData);
-
-% Set up output data and get truth or predicted midpoint function
-[ci, cs]  = deal(zeros([size(nxi,1) , 2, size(nxi,3)]));
-[mi, mc]  = deal([]);
-mid_truth = zeros(size(mids_truth));
-hlfSeg    = ceil(size(nxi,1)/2);
-midIdx    = extractIndices(idx, ttlSegs, []);
-midFnc    = getMidpointFunction(req, crv, pMids, midIdx);
-pVector   = pMids(midIdx, :);
-
-% Convert midpoints to to image frame coordinates
-for sIdx = 1 : ttlSegs
-    % Get midpoint coordinate and halfway segment coordinate
-    %     pm                = crv.getParameter('Pmats', sIdx);
-    pm                = reconstructPmat(req, crv, pVector, sIdx);
-    mid_truth(sIdx,:) = midFnc(sIdx);
-    iSeg              = [nxi(:,sIdx) nyi(:,sIdx)];
-    sSeg              = [nxs(:,sIdx) nys(:,sIdx)];
-    
-    % Convert to image frame coordinates
-    ci(:,:,sIdx) = reverseMidpointNorm(iSeg, pm) + mid_truth(sIdx,:);
-    cs(:,:,sIdx) = reverseMidpointNorm(sSeg, pm) + mid_truth(sIdx,:);
-    mi(:,:,sIdx) = reverseMidpointNorm(iSeg(hlfSeg,:), pm) + mid_truth(sIdx,:);
-    mc(:,:,sIdx) = reverseMidpointNorm(sSeg(hlfSeg,:), pm) + mid_truth(sIdx,:);
+%% Check if data to plot is in training or testing set
+chk = ismember(idx, trnIdx);
+if chk
+    tSet = 'in training set';
+    fSet = 'training';
+else
+    tSet = 'in testing set';
+    fSet = 'testing';
 end
 
-%% Synthesized midpoint contours
-mi = squeeze(mi)';
-mc = squeeze(mc)';
-mi = [mi ; mi(1,:)];
-mc = [mc ; mc(1,:)];
-
-syntHalf_inp = mi;
-syntHalf_sim = mc;
-
-%% Plot all segments for single contour [converted to image frame]
+%% Show ground truth Z-Vectors and Contours
+subplot(row , col , pIdx); pIdx = pIdx + 1;
 imagesc(img);
 colormap gray;
 axis image;
-axis off;
 hold on;
 
-% Plot segments [debug mode]
-%     for l = 1 : ttlSegs
-%         plt(ci(:,:,l), 'b-', 2);
-%         plt(cs(:,:,l), 'r-', 2);
-%     end
+arrayfun(@(x) plt(Zin.M(x,:), 'g.', 3), sIdxs, 'UniformOutput', 0);
+arrayfun(@(x) plt([Zin.M(x,:) ; Zin.T(x,:)], 'b-', 1), ...
+    sIdxs, 'UniformOutput', 0);
+arrayfun(@(x) plt([Zin.M(x,:) ; Zin.N(x,:)], 'r-', 1), ...
+    sIdxs, 'UniformOutput', 0);
 
-% Plot midpoint skeletons, halfway coodinates, and predictions
-% plt(mi,  'g-' , 2);  % Inputted halfway coordinates from PCA
-% plt(mc,  'y-' , 2);  % Simulated halfway coordinates from PCA
-plt(mids_truth, 'g.' , 10); % Actual midpoint coordinates
-plt(mid_truth, 'r+' , 8);  % Requested midpoint coordinates
-
-ttl = sprintf('Prediction Assessment [%s]\n%d x-PCs | %d y-PCs | %d z-PCs\nContour %d Backbone onto Contour %d Image', ...
-    req, pcx, pcy, pcz, idx, idx);
+plt(ht, 'g--', 1);
+ttl = sprintf('Contour from Z-Vectors\nTruth\nContour %d [%s]', ...
+    cIdx, tSet);
 title(ttl);
 
+%% Show predicted Z-Vectors and Contours
+subplot(row , col , pIdx); pIdx = pIdx + 1;
+imagesc(img);
+colormap gray;
+axis image;
+hold on;
+
+arrayfun(@(x) plt(Zout.M(x,:), 'g.', 3), sIdxs, 'UniformOutput', 0);
+arrayfun(@(x) plt([Zout.M(x,:) ; Zout.T(x,:)], 'b-', 1), ...
+    sIdxs, 'UniformOutput', 0);
+arrayfun(@(x) plt([Zout.M(x,:) ; Zout.N(x,:)], 'r-', 1), ...
+    sIdxs, 'UniformOutput', 0);
+
+plt(hp, 'g--', 1);
+ttl = sprintf('Contour from Z-Vectors\nPredicted\nContour %d [%s]', ...
+    cIdx, tSet);
+title(ttl);
+
+%% Overlay Ground Truth Vs Predicted Z-Vector
+subplot(row , col , pIdx); pIdx = pIdx + 1;
+imagesc(img);
+colormap gray;
+axis image;
+hold on;
+
+arrayfun(@(x) plt(mdt(x,:), 'g.', 3), sIdxs, 'UniformOutput', 0);
+arrayfun(@(x) plt([mdt(x,:) ; tnt(x,:)], 'b--', 1), sIdxs, 'UniformOutput', 0);
+arrayfun(@(x) plt([mdt(x,:) ; nrt(x,:)], 'r--', 1), sIdxs, 'UniformOutput', 0);
+
+arrayfun(@(x) plt(mdp(x,:), 'go', 3), sIdxs, 'UniformOutput', 0);
+arrayfun(@(x) plt([mdp(x,:) ; tnp(x,:)], 'b-', 1), sIdxs, 'UniformOutput', 0);
+arrayfun(@(x) plt([mdp(x,:) ; nrp(x,:)], 'r-', 1), sIdxs, 'UniformOutput', 0);
+ttl = sprintf('Z-Vectors only\nTruth Vs. Predicted\nContour %d [%s]', ...
+    cIdx, tSet);
+title(ttl);
+
+%% Overlay Ground Truth Vs Predicted Contour
+subplot(row , col , pIdx); pIdx = pIdx + 1;
+imagesc(img);
+colormap gray;
+axis image;
+hold on;
+
+plt(ht, 'g--', 1);
+plt(hp, 'y-', 1);
+ttl = sprintf('Contour only\nTruth Vs. Predicted\nContour %d [%s]', ...
+    cIdx, tSet);
+title(ttl);
+
+%% Save figure as .fig and .tif
 if sav
-    fnm = sprintf('%s_PredictionAssessment_Contour%dContour%d_%s', ...
-        tdate('s'), idx, idx, req);
-    savefig(fig, fnm);
-    saveas(fig, fnm, 'tiffn');
+    fnms{fIdx} = sprintf('%s_TruthVsPredictedContours_Contour%d_%s', ...
+        tdate('s'), cIdx, fSet);
+    savefig(figs(fIdx), fnms{fIdx});
+    saveas(figs(fIdx), fnms{fIdx}, 'tiffn');
 else
-    pause(0.5);
+    pause(1);
 end
 
 end
@@ -129,25 +136,28 @@ switch req
 end
 
 end
-function pm = reconstructPmat(req, crv, zVec, sIdx)
+
+function pm = getPmat(req, crv, zVec, sIdx)
 %% reconstructPmat: return ground truth or generate the predicted Pmat
-% Returns the ground truth Pmat directly from the Curve object or generates a 
-% Pmat from the predicted Z-Vector. 
+% Returns the ground truth Pmat directly from the Curve object or generates a
+% Pmat from the predicted Z-Vector.
 switch req
     case 'truth'
         pm = crv.getParameter('Pmats', sIdx);
         
     case 'predicted'
-        M    = zVec(1:2);
-        T    = zVec(3:4) - M; % Pmats have T and N subtracted by M
-        N    = zVec(5:6) - M; % Pmats have T and N subtracted by M
-        tF   = [T(1) , T(2) , 0 ; ...
-                N(1) , N(2) , 0 ; ...
-                0    ,  0   , 1 ];
-        mF   = [1    , 0    , -M(1) ; ...
-                0    , 1    , -M(2) ; ...
-                0    , 0    , 1 ];
-        pm   = tF * mF;
+        pm = reconstructPmat(zVec(sIdx,:));
+        %
+        %         M    = zVec(1:2);
+        %         T    = zVec(3:4) - M; % Pmats have T and N subtracted by M
+        %         N    = zVec(5:6) - M; % Pmats have T and N subtracted by M
+        %         tF   = [T(1) , T(2) , 0 ; ...
+        %                 N(1) , N(2) , 0 ; ...
+        %                 0    ,  0   , 1 ];
+        %         mF   = [1    , 0    , -M(1) ; ...
+        %                 0    , 1    , -M(2) ; ...
+        %                 0    , 0    , 1 ];
+        %         pm   = tF * mF;
         
     otherwise
         % Default to ground truth value
