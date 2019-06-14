81118757a3b24fc9407014fb2befbd58b872e94b
Julian Bustamante <jbustamante35@gmail.com>
2019-06-13 22:01:41 -0500

    Pipeline for selecting training set images and automated training
    Former-commit-id: 42f2ca1dfaa05d8247a4129120f9598d4ddb5e06

--- helpers/training/getTrainingIndex.m
+++ helpers/training/getTrainingIndex.m
@@ -1,4 +1,4 @@
-function I = getTrainingIndex(Ein, N, rng)
+function I = getTrainingIndex(Ein, N, rng, minf)
 %% getTrainingIndex: returns matrix of indices to serve as training data input
 % This function takes in an Experiment object and randomly extracts a matrix of
 % indices to serve as input for the trainCircuits function. It searches through
@@ -12,6 +12,7 @@ function I = getTrainingIndex(Ein, N, rng)
 %   Ein: Experiment object with Genotype and Seedling objects
 %   N: total number of objects to train
 %   rng: percentage range within a Seedling's lifetime to extract frame numbers
+%   minf: minimum number of frames in Lifetime to draw from [recommended 20]
 %   
 % Output:
 %   I: [N x 3] matrix representing [Genotype Seedling frame] to train
@@ -21,21 +22,31 @@ function I = getTrainingIndex(Ein, N, rng)
 m = @(x) randi([1 length(x)], 1);
 M = @(x) x(m(x));
 
-%% Get random Genotypes from Experiment
-gIdx = randi([1 Ein.NumberOfGenotypes], [N 1]);
+%% Filter out Genotypes with too few images and draw random set
+gg   = cell2mat(arrayfun(@(x) Ein.getGenotype(x).TotalImages >= minf, ...
+    1 : Ein.NumberOfGenotypes, 'UniformOutput', 0));
+gIdx = randi([1 numel(gg)], [N 1]);
 g    = Ein.getGenotype(gIdx);
 
-%% Get random Seedling from each Genotype
-sIdx = arrayfun(@(x) m(x.getSeedling(':')), g, 'UniformOutput', 0);
-sIdx = cat(1, sIdx{:});
+%% Filter out Seedlings with too few frames and draw random set
+sg = arrayfun(@(y) ...
+    cell2mat(arrayfun(@(x) y.getSeedling(x).Lifetime >= minf, ...
+    1 : y.NumberOfSeedlings, 'UniformOutput', 0)), g, 'UniformOutput', 0);
+sIdx = cell2mat(cellfun(@(x) m(x == 1), sg, 'UniformOutput', 0))';
 s    = arrayfun(@(x) g(x).getSeedling(sIdx(x)), 1:N, 'UniformOutput', 0);
 
 %% Get random frames within range of percentages for each Seedling
-fIdx = cellfun(@(x) M(ceil(rng(1) * x.Lifetime) : ceil(rng(2) * x.Lifetime)), ...
+% Get array of untrained frames within selected range in lifetime
+hIdx = cellfun(@(x) x.MyHypocotyl.getUntrainedFrames, s, 'UniformOutput', 0);
+rIdx = cellfun(@(x) ceil(rng(1) * x.Lifetime) : ceil(rng(2) * x.Lifetime), ...
     s, 'UniformOutput', 0);
+
+% Select a random untrained frame 
+fIdx = cellfun(@(r,h) M(r(ismember(r, h))), ...
+    rIdx, hIdx, 'UniformOutput', 0);
 fIdx = cat(1, fIdx{:});
 
 %% Combine indices to get final output matrix
-I = [gIdx sIdx fIdx];
+I = unique(sortrows([gIdx sIdx fIdx]), 'rows', 'stable');
 
 end
