115c39e88fee8c35889e050a6aaeeeaccda3ac36
Julian Bustamante <jbustamante35@gmail.com>
2019-08-22 00:26:37 -0500

    Updating Curve class to optimize getting S and Z Patches, trimmed object data

--- helpers/classhelp/envelopeMethod.m
+++ helpers/classhelp/envelopeMethod.m
@@ -1,8 +1,9 @@
 function env = envelopeMethod(crd, seg, maxD)
-%% envelopeMethod: converts midpoint-normalized coordinates to coordinates within the envelope
-% This function converts normalized segment coordinates to its coordinate within the envelope of
-% that segment. The [x y] coordinates outputted correspond to the normalized distance
-% between starting and ending points (x) and normalized the distance from the center line (y).
+%% envelopeMethod: converts midpoint-normalized to envelope coordinates 
+% This function converts midpoint-normalized segment coordinates to its 
+% coordinate within the envelope of that segment. The [x y] resulting 
+% coordinates correspond to the normalized distance between starting and ending 
+% points (x) and normalized the distance from the center line (y).
 %
 % Usage:
 %   env = envelopeMethod(crd, seg, dOut, dInn)
@@ -11,8 +12,6 @@ function env = envelopeMethod(crd, seg, maxD)
 %   crd: coordinates of the segment to convert to envelope coordinates
 %   seg: true segment to compare to
 %   maxD: maximum distance from curve to either envelope
-%   dOut: maximum distance vector from the curve to outer envelope [old]
-%   dInn: maximum distance vector from the curve to inner envelope [old]
 %
 % Output:
 %   env: converted coordinates in envelope format
--- helpers/contours/distanceFromCurve.m
+++ helpers/contours/distanceFromCurve.m
@@ -1,40 +1,37 @@
 function dst = distanceFromCurve(envelope_coordinate, curve, maxD)
-%% distanceFromCurve: compute normalized distance from a point in an envelope to the curve segment
-% This function calculates the normalized distance [-1, 1] of a  point within the envelope of a
-% Curve object to the main segment of the curve.
+%% distanceFromCurve: normalized distance from envelope point to curve segment
+% This function calculates the normalized distance [-1, 1] of a  point within 
+% the envelope of a Curve object to the main segment of the curve.
 %
-% The equation for this conversion is fairly simple but could be confusing for me in the future:
+% The equation for this conversion is fairly simple but could be confusing for 
+% me in the future:
 %   env_env_y = (maxD - ( env_norm_y - ( curve_norm_y - maxD ))) / maxD
 %
 % Where:
 %   env_env_y: envelope's y-coordinate represented in envelope coordinates
-%   env_norm_y: envelope's y-coordinate represented in midpoint-normalized coordinates
-%   curve_norm_y: original curve's y-coordinate represented in midpoint-normalized coordinates
+%   env_norm_y: envelope's y-coordinate in midpoint-normalized coordinates
+%   curve_norm_y: curve's y-coordinate in midpoint-normalized coordinates
 %   maxD: maximum distance from the original curve to the left or right envelope
 %
 % Usage:
 %   dst = distanceFromCurve(envelope_coordinate, curve, maxD)
 %
 % Input:
-%   envelope_coordinate: coordainte within envelope to compute distance from curve-coordinate
+%   envelope_coordinate: envelope coordinate to compute distance from curve
 %   curve: main segment to compute distance from
 %   maxD: maximum distance from the main segment to the extent of the envelope
 %
 
 N2E_Y = @(a,b,c) (a - ( b - ( c - a ))) ./ a;
-dst   = N2E_Y(maxD, getDim(envelope_coordinate, 2), findIndex(curve, envelope_coordinate));
+dst   = N2E_Y(maxD, getDim(envelope_coordinate, 2), ...
+    findIndex(curve, envelope_coordinate));
 
 end
 
 function idx = findIndex(curve, envelope_coordinate)
-%% findIndex: subfunction to find index along curve corresponding to envelope coordinate
+%% findIndex: find index along curve corresponding to envelope coordinate
 
 idx = curve(curve(:,1) == envelope_coordinate(:,1), 2);
 
 end
 
-% Old Method
-% max_coordinate    = curve(curve(:,1) == envelope_coordinate(:,1), 2) - maxD;
-% distance_from_max = envelope_coordinate - max_coordinate;
-% dst               = (maxD - distance_from_max) / maxD;
-% dst               = getDim(dst, 2);
