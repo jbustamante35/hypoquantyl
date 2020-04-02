1389a55560e88cd3ca342e599e5c5c447387ce7c
Julian Bustamante <jbustamante35@gmail.com>
2020-04-02 14:25:08 -0500

    Better documentation for variance_explained

--- helpers/pca/multi_pca/variance_explained.m
+++ helpers/pca/multi_pca/variance_explained.m
@@ -1,22 +1,22 @@
 function [V, T] = variance_explained(E, P)
-%% variance_explained: calculate variance explained from given data set D
-% This function takes the eigenvalues of a dataset after PCA to assess the
-% variance explained by each principal component. Eigenvalues should be inputted
-% as a diagonal matrix.
+%% variance_explained: compute variance explained from PCA
+% This function takes the eigenvalues from PCA to compute the variance
+% explained by each principal component. Eigenvalues should be inputted as a
+% diagonal matrix, and not as a vector.
 %
 % Usage:
 %   [V, T] = variance_explained(E, P)
 %
 % Input:
-%   E: eigenvalues from the dataset to assess variance
-%   P: percentage of explained variance to cut-off
+%   E: eigenvalues from PCA
+%   P: cut-off percentage to cpmute explained variance
 %
 % Output:
-%   V: variance explained from eigenvalues of dataset E
-%   T: number of components equaling the cutoff percentage P
+%   V: variance explained
+%   T: number of PCs at the cut-off percentage
 %
 
-%% Cumulative sum of each variance over the sum of all the variances 
+%% Cumulative sum of each variance over the sum of all the variances
 % Default to 100% variance explained
 if nargin < 2
     P = 1;
