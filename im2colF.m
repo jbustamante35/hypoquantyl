64bbb8af39ccbcdb9712625bfb78eb1834223e3e
Julian Bustamante <jbustamante35@gmail.com>
2024-10-11 14:47:37 -0500

    Fix for uncompiled im2colF

---
+++ im2colF.m
@@ -0,0 +1,11 @@
+function Z = im2colF(F, X, Y)
+%% im2colF: rearrange image blocks into columns
+
+try
+    % Try compiled version
+    Z = im2colF_c(F, X, Y);
+catch
+    % Default to matlab's im2col
+    Z = im2col(F, X, Y);
+end
+end
